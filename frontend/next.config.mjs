764329de490c2543ab992955f04615f906f65a34
Zeeshan Ilahi <zeeilahi@iu.edu>
2025-04-28 16:35:38 -0400

    adding images on s3

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images : {
+        remotePatterns : [
+            {
+                protocol: 'https',
+                hostname: 'pm-s3-image.s3.us-east-1.amazonaws.com',
+                port: "",
+                pathname: "/**"
+            }
+        ]
+    }
+};
 
 export default nextConfig;
--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -60,7 +60,7 @@ const Sidebar = () => {
         {/* TEAM */}
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
           <Image
-            src="/logo.png"
+            src="https://pm-s3-image.s3.us-east-1.amazonaws.com/logo.png"
             alt="logo"
             width={40}
             height={40}
